278be25791c517ee428710bbc733bd73d95fd456
Diulia C. Godoy Maciel <diuliac.pf@gmail.com>
2022-06-23 11:42:30 -0300

    att aula dois

--- nest-api/src/app.service.ts
+++ nest-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Se mostrar essa mensagem é pq tá rodando certo!';
   }
 }
